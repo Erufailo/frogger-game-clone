22c627688ee8bdeae27e792cf3c58c30d909e64f
erufailo <jrin2113@gmail.com>
2018-04-06 16:05:35 +0300

    Feat: Create the enemy movement and speed

--- js/app.js
+++ js/app.js
@@ -1,40 +1,69 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function () {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = -150;
+    this.y = 225;
+    this.speed = 5;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+   
+    this.x += this.speed;
+    if (this.x > 600){
+        this.speed = getRandomNumberOf(12);
+        this.x = -150;
+        this.y = randomEnemyPosition();
+    }
+    return dt * this.x;
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor() {
 
+    }
+
+    update() {
+
+    }
+    render() {
+
+    }
+    handleInput() {
+
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let player = new Player();
+let allEnemies = [];
+allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
+allEnemies.push(new Enemy());
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -44,3 +73,18 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+function randomEnemyPosition(){
+    let x= getRandomNumberOf(3);
+    if(x===1){
+        return 60;
+    }else if(x===2){
+        return 145;
+    }else{
+        return 225;
+    }
+}
+
+function getRandomNumberOf(x) {
+   return Math.floor(Math.random() * x) + 1;
+}
